c939700c7c6e9a53405e222398987a74b214c9c5
ömür Gün <mobile@bod.com.tr>
2021-03-12 17:52:25 +0300

    commit

---
+++ LearnMvvmCryptoMoney/ViewModel/CryptoViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CryptoViewModel.swift
+//  LearnMvvmCryptoMoney
+//
+//  Created by Sakir Bayram on 12.03.2021.
+//
+
+import Foundation
+
+struct CryptoListViewModel {
+    let cryptoCurrencyList: [CryptoCurrency]
+    
+    func numberOfRowsInSection() -> Int {
+         return self.cryptoCurrencyList.count
+     }
+     
+     func cryptoAtIndex(_ index: Int) -> CryptoViewModel {
+         let crypto = self.cryptoCurrencyList[index]
+         return CryptoViewModel(crypto)
+     }
+}
+
+
+struct CryptoViewModel {
+    let crytpoCurrency: CryptoCurrency
+    
+    init(_ crypto: CryptoCurrency) {
+        self.crytpoCurrency = crypto
+    }
+    
+    var name: String {
+        return self.crytpoCurrency.currency
+    }
+    
+    var price: String {
+        return self.crytpoCurrency.price
+    }
+}
+
