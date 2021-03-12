e6fd57bd1357ca8295149e95274a4b46f49d9698
ömür Gün <mobile@bod.com.tr>
2021-03-12 16:22:31 +0300

    commit

--- LearnMvvmCryptoMoney/ViewController.swift
+++ LearnMvvmCryptoMoney/ViewController.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+    }
+    
 
 
 }
